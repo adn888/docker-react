600a843bdf55628fba5b3cd34e257eb1e89d343d
nguyenan <nguyenan@localhost.localdomain>
2020-02-27 11:29:37 +1100

    add change

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> 2JZ no shit
+          I LOVE LADYBOYS!
         </p>
         <a
           className="App-link"
